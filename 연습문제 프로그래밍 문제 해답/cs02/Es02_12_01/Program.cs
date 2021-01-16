c3cfc94639942cc271dc627d9e7570e93e9ef3f7
MEAJIN <kimo3o98@gmail.com>
2021-01-16 22:26:53 +0900

    ;-)

---
+++ 연습문제 프로그래밍 문제 해답/cs02/Es02_12_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es02_12_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, l;
+            double s;
+            Console.Write("Enter a : ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Enter b : ");
+            b = float.Parse(Console.ReadLine());
+            Console.Write("Enter c : ");
+            c = float.Parse(Console.ReadLine());
+            l = (a + b + c) / 2;
+            Console.WriteLine("l : " + l);
+            s = Math.Sqrt(Math.Pow(l - a, 2) + Math.Pow(l - b, 2) + Math.Pow(l - c, 2));
+            Console.WriteLine("s : " + s);
+        }
+    }
+}
